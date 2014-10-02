c724725448c0d9b2c7f9dfb31ccc2ce2fa76064f
datoncito <campitos@mexico-movil.com>
2014-10-02 11:03:08 -0500

    Horoscopo

--- src/com/capitulo2/encapsulamiento/SistemaDePago2.java
+++ src/com/capitulo2/encapsulamiento/SistemaDePago2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.capitulo1.abstraccion;
+package com.capitulo2.encapsulamiento;
 
 import com.capitulo1.abstraccion.*;
 import com.capitulo2.encapsulamiento.Usuario;
@@ -13,18 +13,17 @@ import com.capitulo2.encapsulamiento.Pago;
  *
  * @author campitos
  */
-public class SistemaDePago {
+public class SistemaDePago2 {
 
 
     public static void main(String args[]){
- System.out.println(hacerPago(40,200));
+     Usuario u = new Usuario();
+     u.setNombre("pedro");
+     u.setHorasTrabajadas(40);
+     u.setSueloBase(3000); 
+     Pago p=new Pago();
+     p.hacerPago(u);
     }
-     public static float hacerPago(int horas, float sueldo){
-        
-        float pago=horas*sueldo;
     
-        return pago;
-        
-    }
     
 }
--- src/com/capitulo2/encapsulamiento/Usuario.java
+++ src/com/capitulo2/encapsulamiento/Usuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.capitulo1.abstraccion;
+package com.capitulo2.encapsulamiento;
 
 /**
  *
